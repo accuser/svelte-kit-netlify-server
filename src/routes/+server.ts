61a9cae4e743e4326bf28bc8c094f84d73301e19
Matthew Gibbons <matthew@southwestkoala.co.uk>
2022-09-16 08:22:34 +0100

    Sanity

--- src/routes/+server.ts
+++ src/routes/+server.ts
@@ -1,5 +1,18 @@
 import type { RequestHandler } from './$types';
+import SanityClientConstructor from '@sanity/client';
+import groq from 'groq';
+import { SANITY_DATASET, SANITY_PROJECT_ID, SANITY_TOKEN } from '$env/static/private';
 
-export const GET: RequestHandler = () => {
-	return new Response('Hellow, World!');
+export const GET: RequestHandler = async () => {
+	const sanity = SanityClientConstructor({
+		dataset: SANITY_DATASET,
+		projectId: SANITY_PROJECT_ID,
+		apiVersion: 'v2021-10-21',
+		token: SANITY_TOKEN,
+		useCdn: true
+	});
+
+	const channels = await sanity.fetch(groq`*[_type == "channel"]`, {});
+
+	return new Response(JSON.stringify(channels));
 };
